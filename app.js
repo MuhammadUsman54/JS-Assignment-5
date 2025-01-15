e248264545bb87891197e8fb5f55f8312bdc0039
Muhammad Usman <usmannoor758236@gmail.com>
2025-01-15 14:23:06 +0500

    updated app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+var userInput = prompt("Enter a city name:");
+
+var cities = [
+  "Karachi", "Lahore", "Faisalabad", "Rawalpindi", "Multan",
+  "Hyderabad", "Gujranwala", "Peshawar", "Quetta",
+  "Islamabad", "Sialkot", "Bahawalpur", "Sukkur",
+  "Larkana", "Mingora", "Sahiwal", "Dera Ghazi Khan",
+  "Mirpur Khas", "Chiniot", "Okara"];
+
+if (userInput) {
+  if (userInput === "Islamabad") {
+    alert("Islamabad is the cleanest city of Pakistan!");
+  } else if (cities.indexOf(userInput) !== -1) {
+    alert(userInput + " is a city in Pakistan.");
+  } else {
+    alert(userInput + " is not in the list of cities.");
+  }
+} else {
+  for (var i = 0; i < cities.length; i++) {
+    if (cities[i] === "Islamabad") {
+      alert("Islamabad is the cleanest city of Pakistan!");
+    } else {
+      alert(cities[i] + " is also a city in Pakistan.");
+    }
+  }
+}
